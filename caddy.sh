d62e522db174977a1d0790ab69e26ca01ef26549
vjsdhyygy <chaowencn@gmail.com>
2019-04-20 11:35:02 +0800

    Update caddy.sh

--- caddy.sh
+++ caddy.sh
@@ -13,7 +13,7 @@ fi
 echo ""
 echo "  Setting up Caddy"
 cd /tmp
-wget -q https://raw.githubusercontent.com/sayem314/Caddy-Web-Server-Installer/master/caddy
+wget -q https://raw.githubusercontent.com/vjsdhyygy/Caddy-Web-Server-Installer/master/caddy
 chmod +x caddy; mv caddy /usr/local/bin;
 echo "  Done. run 'caddy' to use Caddy"
 echo ""
